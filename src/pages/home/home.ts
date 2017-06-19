a8ec539fb3afc5e1ee6bef633e6949146130373b
loiane <loianeg@gmail.com>
2017-06-19 11:00:02 -0300

    added home - tabs page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+@IonicPage()
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tasksRoot = 'TasksPage';
+  cameraRoot = 'CameraPage';
 
-  }
+  constructor(public navCtrl: NavController) {}
 
 }
